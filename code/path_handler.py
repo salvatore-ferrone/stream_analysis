5aee33e81b29d6a16bcafd3ff0c34028698efe11
sferrone <salvatore.ferrone@obspm.fr>
2024-05-30 11:05:07 +0200

    more path changes

--- code/path_handler.py
+++ code/path_handler.py
@@ -11,26 +11,35 @@ This code will be used to obtain base paths and also derived paths to output fil
 import json
 import os 
 
-
 base = json.load(open('../paths.json'))
 
 
 def temporary_tau_coordiantes_folder(GCname:str, montecarlokey:str,potential_stream_orbit:str):
-    return base["temporary"] +"stream-1D-density/"+potential_stream_orbit+"/"+GCname+"/"+montecarlokey+"/"
+    path=base["temporary"] +"stream-1D-density/"+potential_stream_orbit+"/"+GCname+"/"+montecarlokey+"/"
+    os.makedirs(path,exist_ok=True)
+    return path
 
-
-def tau_coordinates(GCname:str,montecarlokey:str):
-    outpath="/scratch2/sferrone/intermediate-products/stream_density_profiles/" + GCname + "/"
-    return outpath + GCname+"_time_profile_density_"+montecarlokey+".hdf5"
+def tau_coordinates(GCname:str,montecarlokey:str,potential_stream_orbit:str):
+    path = base['intermediate-products'] + "stream_tau_projections/" + potential_stream_orbit +"/"+ GCname + "/"
+    name = GCname+"_tau_projections_"+montecarlokey+".hdf5"
+    os.makedirs(path,exist_ok=True)
+    return path+name
 
 def force_on_orbit(GCname:str,montecarlokey:str,orbit_potential_name:str):
-    return base['ForceOnOrbit'] + orbit_potential_name +"/"+ GCname + "/Pal5-"+montecarlokey+"-suspects-FORCEONORBIT.hdf5"
+    path = base['ForceOnOrbit'] + orbit_potential_name +"/"+ GCname + "/"
+    name = GCname+"-"+montecarlokey+"-suspects-FORCEONORBIT.hdf5"
+    os.makedirs(path,exist_ok=True)
+    return path+name
 
-def stream_orbit(gcname:str,montecarlokey:str,potential_name:str,NP:int):
-    return base['StreamOrbits'] +potential_name +"/"+ gcname + "/"+ str(NP) + "/Pal5-"+montecarlokey+"-StreamOrbit.hdf5"
+def stream_orbit(GCname:str,montecarlokey:str,potential_name:str,NP:int):
+    path = base['StreamOrbits'] +potential_name +"/"+ GCname + "/"+ str(NP) + "/"
+    name = GCname+"-"+montecarlokey+"-StreamOrbit.hdf5"
+    os.makedirs(path,exist_ok=True)
+    return path+name
 
-def orbit(montecarlokey:str,gcname:str,potential_name:str):
-    return base['Orbits'] + potential_name +"/"+ gcname + "-orbits.hdf5"
+def orbit(GCname:str,potential_name:str):
+    path = base['Orbits'] + potential_name +"/"
+    name = GCname + "-orbits.hdf5"
+    os.makedirs(path,exist_ok=True)
+    return path+name
 
-def stream_density_profiles_path(gcname:str,montecarlokey:str):
-    return base['intermediate-products'] +"stream_density_profiles/"+gcname + "/"+ gcname + "_time_profile_density_" + montecarlokey + ".hdf5"
